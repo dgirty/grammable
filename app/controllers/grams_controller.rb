e70b430c9b3efaac8c5edecf7ad34d4ce4a793fd
dgirty <dggirty@gmail.com>
2019-09-27 20:57:06 +0000

    TDD-18

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,10 +1,6 @@
 class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
-  def index
-    @grams = Gram.all
-  end
-
   def new
     @gram = Gram.new
   end
@@ -16,6 +12,10 @@ class GramsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
+  
+  def index
+    @grams = Gram.all
   end
 
   def show
@@ -33,34 +33,27 @@ class GramsController < ApplicationController
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
     return render_not_found(:forbidden) if @gram.user != current_user
-
     @gram.update_attributes(gram_params)
     if @gram.valid?
       redirect_to root_path
     else
       return render :edit, status: :unprocessable_entity
-    end
+    end 
   end
 
   def destroy
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
-    return render_not_found(:forbidden) if @gram.user != current_user
+      return render_not_found(:forbidden) if @gram.user != current_user
     @gram.destroy
     redirect_to root_path
   end
-
 
   private
 
   def gram_params
     params.require(:gram).permit(:message, :picture)
   end
-
-
-  def render_not_found(status=:not_found)
-    render plain: "#{status.to_s.titleize} :(", status: status
-  end
-
+  
 end
 
